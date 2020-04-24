0b9cef129fc6bfc951abd7f43d9912ee1bad2022
DongCX-LDHSP <52726994+DongCX-LDHSP@users.noreply.github.com>
2020-04-24 21:53:18 +0800

    静态链接Done！

---
+++ source/lib.h
@@ -0,0 +1,4 @@
+/* This file declare the functions fred and bill for users. */
+
+void bill(char*);
+void fred(int);
---
+++ source/slmain.c
@@ -0,0 +1,7 @@
+#include "lib.h"
+
+int main()
+{
+    bill("Hello world");
+    return 0;
+}
